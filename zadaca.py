c1da101ef1b53a8c3401cf0f08c611a24ba22777
Mitar Zirojevic <mitarzirojevic609@gmail.com>
2026-02-11 20:36:56 +0100

    Prva python vezba

---
+++ zadaca.py
@@ -0,0 +1,96 @@
+from itertools import chain
+
+from main import name
+#zadatak 1
+name = ("Mitar")
+lastname =("Zirojevic")
+print(name, lastname)
+#zadatak 2
+godina_rodjenja = 1998
+trenutna_godina = 2025
+starost = trenutna_godina - godina_rodjenja
+print(starost)
+#zadatak3
+ceo_broj = 25
+decimalni_broj = 25.5
+text =("ovo je string")
+print(type (ceo_broj))
+print(type (decimalni_broj))
+print(type (text))
+#zadatak 4
+a=12
+b=8
+print(a+b)
+#zadatak 5
+x=50
+y=18
+print(x-y)
+#zadatak 6
+cena = 25
+kolicina = 4
+print(cena * kolicina)
+#zadatak 7
+ukupno = 100
+ljudi = 4
+print(ukupno / ljudi)
+#zadatak 8
+a=5
+print(a*a)
+#zadatak 9
+a = 5
+b = 6
+c = 7
+prosjek = (a+b+c) /3
+print(prosjek)
+#zadatak 10
+broj = 10
+print(broj)
+broj = 25
+print(broj)
+#zadatak 11
+grad = ("Sarajevo")
+drzava = ("Bosna i Hercegovina")
+lokacija = grad + ", " + drzava
+print(lokacija)
+#zadatak 12
+ime = "Mitar"
+Prezime = "Zirojevic"
+puno_ime = ime + ", " + Prezime
+print(puno_ime)
+#zadatak 13
+auto = "Fiat"
+duzina = len(auto)
+print(duzina)
+#zadatak 14
+cena = 100
+porez = 20
+cena_sa_porezom = cena+(cena*porez/100)
+print(cena_sa_porezom)
+#zadatak 15 nisam bas skontao na koju se cijenu misli
+#zadatak 16
+varijabla_1 = 3; varijabla_2 = 4; varijabla_3 = 5
+print(varijabla_1, varijabla_2, varijabla_3)
+#zadatak 17
+a = 7
+b = 3
+a,b = b,a
+print(a, b)
+#18 zadatak
+cena = 80
+poruka = f"Proizvod košta {cena} dinara."
+print(poruka)
+#zadatak19
+a = 5
+b = 3
+P = a*b
+print(P)
+a = 5
+b = 3
+O= 2*(a+b)
+print(O)
+#20 zadatak
+ime = "flow"
+broj_sati_rada = 8
+cena_po_satu = 600
+ukupna_zarada = broj_sati_rada * cena_po_satu
+print(ukupna_zarada)
